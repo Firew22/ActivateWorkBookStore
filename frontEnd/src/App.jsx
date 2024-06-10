5e338af3665071f0e5b9d017a804d2e16cd418d8
Firew Handiso <firewh18@gmail.com>
2024-06-10 12:25:07 -0600

    created the route dir and all the other important pages

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,7 +1,15 @@
-export default function App() {
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+
+    <>
+    <nav>siteLogo</nav>
+    <Outlet/>
+    
+    </>
   )
 }
+
+export default App
---
+++ frontEnd/src/router/Router.jsx
@@ -0,0 +1,32 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import App from '../App'
+import Home from "../Home/Home";
+import Shop from "../Shop/Shop";
+import About from "../components/About";
+import Blog from "../components/Blog";
+import SingleBook from "../components/SingleBook";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children:[{
+        path: "/",
+        element: <Home/>
+    },
+    {
+        path: "/Shop",
+        element: <Shop/>
+    },
+    {
+        path: "/About",
+        element: <About/>
+    }
+]
+  },
+]);
+export default router;
